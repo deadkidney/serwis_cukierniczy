2294839e5c3c437120e3d2f99e1c4f614fa8512f
deadkidney <jamczylija@gmail.com>
2026-05-10 17:08:34 +0200

    Changed server base.

--- server/server.js
+++ server/server.js
@@ -7,15 +7,48 @@ const corsOptions = {
 const app = express();
 app.use(cors(corsOptions));
 
-app.get("/api", (req, res) => {
-	res.json({ message: "Hello from server!" });
+let shortRecipes = [{
+    id: 1,
+    title: "przepis",
+}, {
+    id: 2,
+    title: "przepis 2",
+}, {
+    id: 3,
+    title: "przepis 3",
+}, {
+    id: 4,
+    title: "przepis 4",
+}];
+
+let recipes = [{
+    id: 1,
+    title: "przepis",
+    ingredients: [{
+        id: 0, name: "jajko", amount: 3, unit: "sztuka"
+    }, {
+        id: 1, name: "cukier", amount: 15, unit: "g"
+    }]
+}, {
+    id: 2,
+    title: "przepis 2",
+    ingredients: [{
+        id: 0, name: "mąka", amount: 340, unit: "g"
+    }, {
+        id: 1, name: "mleko", amount: 100, unit: "ml"
+    }]
+}];
+
+app.get("/recipes", (req, res) => {
+	res.json({ recipes: recipes });
 });
 // try catch
 
-app.get('/userinfo', (req, res) => { 
-    var username = req.query.id;
-	if (username==1)
-		res.json({message: "yay"});
+
+app.get('/recipeinfo', (req, res) => { 
+    var id = req.query.id;
+	if (id <= recipes.length)
+		res.json({recipe: recipes[id-1]});
 });
 
 app.listen(8080, () => {
